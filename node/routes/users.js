ad3820ddd86533b23c07281b295f11ecb0b9f534
valeri <valeritsert@gmail.com>
2018-08-14 22:46:20 +0400

    users controller

--- node/routes/users.js
+++ node/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const Users = require('../controllers/users')
 
-module.exports = router;
+const router = express.Router()
+
+router.post('/', Users.signup)
+
+module.exports = router
