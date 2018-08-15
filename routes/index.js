db059cfe86ebb2c34aa2d790fead0524413c631b
valeri <valeritsert@gmail.com>
2018-08-15 15:38:36 +0400

    health API

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const users = require('./users')
 const router = express.Router()
 
 router.use('/users', users)
+router.get('/health', (req, res) => {
+	res.send('OK')
+})
 
 module.exports = router
