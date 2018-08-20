e41ecccc54473110a21ca45ee209cc5d2a4deddd
valeri <valeritsert@gmail.com>
2018-08-20 11:47:14 +0400

    file endpoints

--- controllers/file.js
+++ controllers/file.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const mime = require('mime-types')
+const fs = require('fs')
 
 const create = (req, res) => {
 	res.send({
@@ -5,6 +8,21 @@ const create = (req, res) => {
 	})
 }
 
+const get = (req, res) => {
+	const filePath = path.resolve(`files/${path.resolve(req.params.id)}`)
+
+	if (fs.existsSync(filePath)) {
+	  res.set('Content-Type', mime.lookup(filePath))
+		res.sendFile(filePath)
+  } else {
+  	res.status(404).send({
+  		status: false,
+  		error: 'File does not exist'
+  	})
+  }
+}
+
 module.exports = {
-	create
+	create,
+	get
 }
