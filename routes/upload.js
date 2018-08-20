1b454230c28fc869ce7acdf2d7c1c3a1a264787e
valeri <valeritsert@gmail.com>
2018-08-20 11:02:26 +0400

    do not remove uploaded file

--- routes/upload.js
+++ routes/upload.js
@@ -4,20 +4,19 @@ const { check, validationResult } = require('express-validator/check')
 
 const UploadCtrl = require('../controllers/upload')
 
-var multer = require('multer');
-var fs = require('fs');
+var multer = require('multer')
+var fs = require('fs')
 
 const router = express.Router()
 
 
 router.post('/' ,multer({dest: './uploads/'}).array('uploads', 1), function(req,res,next) {
-  var fileInfo = [];
-  if(req.files.length > 0){
-    req.body.pictures = new Buffer(fs.readFileSync(req.files[0].path)).toString('base64');
+  var fileInfo = []
+  if (req.files.length > 0) {
+    req.body.picture = req.files[0].path
   }
 
-  fs.unlink(req.files[0].path);
-  next();
-}, UploadCtrl.createUpload);
+  next()
+}, UploadCtrl.createUpload)
 
 module.exports = router
