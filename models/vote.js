691c61eb10c14c12d6a1914f11e9db841d585717
valeri <valeritsert@gmail.com>
2018-08-21 10:59:15 +0400

    add post_id to vote schema

--- models/vote.js
+++ models/vote.js
@@ -1,6 +1,6 @@
-var sequelize = require('../config/database')
+const sequelize = require('../config/database')
 
-var Vote = sequelize.define('Votes', {
+const Vote = sequelize.define('Votes', {
   id: {
     type: Sequelize.INTEGER,
     allowNull: false,
@@ -11,19 +11,27 @@ var Vote = sequelize.define('Votes', {
     type: Sequelize.INTEGER,
     allowNull: false
   },
+  post_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
   voteBy: {
     type: Sequelize.INTEGER,
     allowNull: false
   }
-},
-{
+}, {
   classMethods: {
     associate: function (models) {
       models.Votes.belongsTo(models.Category, { constraints: false })
     }
   }
-},
-{
+}, {
+  classMethods: {
+    associate: function (models) {
+      models.Votes.belongsTo(models.User, { constraints: false })
+    }
+  }
+}, {
   classMethods: {
     associate: function (models) {
       models.Votes.belongsTo(models.User, { constraints: false })
