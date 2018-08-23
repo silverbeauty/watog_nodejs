403f53b743b2b183776c7d5c013d0d02e1243d11
valeri <valeritsert@gmail.com>
2018-08-23 18:47:28 +0400

    add user_name and check body during sign_up

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const { check, validationResult, query } = require('express-validator/check')
+const { body, validationResult, query } = require('express-validator/check')
 
 const UserCtrl = require('../controllers/user')
 
@@ -8,21 +8,22 @@ const router = express.Router()
 
 router.post('/'
   , [
-    check('email').isEmail(),
+    body('email').isEmail(),
+    body('user_name').isLength({ min: 3 }).withMessage('user_name must be at least 3 chars long'),
     // password must be at least 5 chars long
-    check('password').isLength({ min: 5 }).withMessage('password must be at least 5 chars long'),
-    check('first_name').isLength({ min: 1 }).withMessage('first_name must should be at least 1 chars long'),
-    check('last_name').isLength({ min: 1 }).withMessage('last_name must be at least 1 chars long'),
-    check('country').isLength({ min: 1 }).withMessage('country must be at least 1 chars long'),
-    check('hospital').isLength({ min: 1 }).withMessage('hospital must be at least 1 chars long'),
+    body('password').isLength({ min: 5 }).withMessage('password must be at least 5 chars long'),
+    body('first_name').isLength({ min: 1 }).withMessage('first_name must should be at least 1 chars long'),
+    body('last_name').isLength({ min: 1 }).withMessage('last_name must be at least 1 chars long'),
+    body('country').isLength({ min: 1 }).withMessage('country must be at least 1 chars long'),
+    body('hospital').isLength({ min: 1 }).withMessage('hospital must be at least 1 chars long'),
     // :TODO phone number regexp should be used
-    check('cell_phone').isLength({ min: 9 }).matches(/^[0-9]+$/).withMessage('cell_phone must be at least 9 chars long and only numbers!')
+    body('cell_phone').isLength({ min: 9 }).matches(/^[0-9]+$/).withMessage('cell_phone must be at least 9 chars long and only numbers!')
   ]
   , UserCtrl.signup)
 
 router.post('/login', [
-  check('email').isEmail(),
-  check('password').isLength({ min: 1 })
+  body('email').isEmail(),
+  body('password').isLength({ min: 1 })
 ], UserCtrl.login)
 
 // Return own profile
