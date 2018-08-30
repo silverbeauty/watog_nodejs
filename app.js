cab479527e9fddd6b38bbaf08a4ee5bcf8f0b7ff
valeri <valeritsert@gmail.com>
2018-08-30 08:57:59 +0400

    load env variables

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
 const fs = require('fs')
+require('dotenv').config()
 
 const indexRouter = require('./routes/index')
 
