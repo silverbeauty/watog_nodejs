368314d39e948e0324fbde5d8f984c1f066d6977
valeri <valeritsert@gmail.com>
2018-09-03 19:28:30 +0400

    use RANDOM instead of RAND

--- controllers/post.js
+++ controllers/post.js
@@ -204,7 +204,7 @@ const query = async (req, res) => {
   if (order && !isRandom) {
     sQuery.order = [[order, direction]]
   } else if (isRandom) {
-    sQuery.order = [Sequelize.fn('RAND')]
+    sQuery.order = [Sequelize.fn('RANDOM')]
   }
 
   const data = await Post.findAll(sQuery)
