592008126f9eae684bf1879309b44553f5e11208
valeri <valeritsert@gmail.com>
2018-09-03 10:25:52 +0400

    health check

--- test/user.test.js
+++ test/user.test.js
@@ -3,7 +3,7 @@ const request = require('supertest')
 
 const app = require('../app')
 
-test('base route', t =>
+test('Health Check', t =>
   request(app)
     .get('/api/health')
     .then(res => t.is(res.status, 200)))
