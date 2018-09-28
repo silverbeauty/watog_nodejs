3b5a19223a8359e205cfe5074190250fb881c85b
valeri <valeritsert@gmail.com>
2018-09-29 00:23:58 +0400

    add member

---
+++ models/member.js
@@ -0,0 +1,22 @@
+const sequelize = require('../config/database')
+
+const Sequelize = require('sequelize')
+
+const Member = sequelize.define('Member', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  room_id: {
+    type: Sequelize.UUID,
+    allowNull: false
+  },
+})
+
+module.exports = Member
