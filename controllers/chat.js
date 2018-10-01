bb51db7460c50248c6615a51d3e72dbefcf0e0b7
valeri <valeritsert@gmail.com>
2018-10-01 08:10:31 +0400

    setup socket in chat

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,3 +1,4 @@
+
 const authenticated = (socket) => {
   //this socket is authenticated, we are good to handle more events from it.
   console.log('hello! ' + socket.decoded_token.name);
@@ -8,6 +9,15 @@ const authenticated = (socket) => {
 
   // TODO:Send message
   // io.to('some room').emit('some event');
+}
+
+
+const setup = (io) => {
+	io.sockets
+  .on('connection', socketioJwt.authorize({
+    secret: process.env.JWT_TOKEN,
+    timeout: 15000 // 15 seconds to send the authentication message
+  })).on('authenticated', authenticated);
 }
 
 module.exports = {
