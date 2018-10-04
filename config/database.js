6c30a9c2e216de88688283d74a23b43924dc70bf
valeri <valeritsert@gmail.com>
2018-10-04 09:19:35 +0400

    use mysql db for production

--- config/database.js
+++ config/database.js
@@ -3,22 +3,27 @@ const Sequelize = require('sequelize')
 const { DB_PATH, TEST_DB_PATH } = require('./path')
 
 console.info('NODE_ENV:', process.env.NODE_ENV)
+let sequelize
 
-const sequelize = new Sequelize('database', null, null, {
-  host: 'localhost',
-  dialect: 'sqlite',
-  operatorsAliases: false,
+if (process.env.NODE_ENV === 'test') {
+  const sequelize = new Sequelize('database', null, null, {
+    host: 'localhost',
+    dialect: 'sqlite',
+    operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
 
-  // SQLite only
-  storage: process.env.NODE_ENV === 'test' ? TEST_DB_PATH : DB_PATH
-})
+    // SQLite only
+    storage: TEST_DB_PATH
+  })
+} else { // production mode
+  sequelize = new Sequelize(process.env.DB_URL)
+}
 
 sequelize
   .authenticate()
